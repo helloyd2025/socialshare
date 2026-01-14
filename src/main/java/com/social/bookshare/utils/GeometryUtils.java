d34b46dc9720bbd71f39ba14066b68f775b8d0d8
helloyd2025 <helloyd2025@gmail.com>
2026-01-14 23:13:24 +0900

    refactor: haversine

--- src/main/java/com/social/bookshare/utils/GeometryUtils.java
+++ src/main/java/com/social/bookshare/utils/GeometryUtils.java
@@ -22,22 +22,18 @@ public class GeometryUtils {
     }
 	
 	/** Calculate Haversine distance between two locations */
-	public static double calculateDistance(double lon1, double lat1, double lon2, double lat2) {
-		double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + 
-                      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        
-        return dist * 60 * 1.1515 * 1609.344; // 미터 단위 변환
+	public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+	    final int R = 6371; // kilometer
+
+	    double dLat = Math.toRadians(lat2 - lat1);
+	    double dLon = Math.toRadians(lon2 - lon1);
+
+	    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	               Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+	               Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    
+	    return R * c * 1000; // meter
 	}
-	
-	private static double deg2rad(double deg) {
-		return (deg * Math.PI / 180.0); 
-	}
-	
-    private static double rad2deg(double rad) {
-    	return (rad * 180 / Math.PI);
-    }
 }
